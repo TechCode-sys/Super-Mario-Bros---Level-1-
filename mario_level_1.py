a6d045b38a67a6a46d7c4e8bd925917056100123
TechCode-sys <62327574+TechCode-sys@users.noreply.github.com>
2020-03-18 20:17:43 +0100

    Update mario_level_1.py

--- mario_level_1.py
+++ mario_level_1.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python
-__author__ = 'justinarmstrong'
-
-"""
-This is an attempt to recreate the first level of
-Super Mario Bros for the NES.
-"""
-
 import sys
 import pygame as pg
 from data.main import main
